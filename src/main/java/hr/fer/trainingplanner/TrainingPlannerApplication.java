c2e9919ffbb0f22ebec6580f642b838aa3fdc4a2
Toni Martinčić <toni.martincic@fer.hr>
2020-10-26 21:46:45 +0100

    Initial commit

---
+++ src/main/java/hr/fer/trainingplanner/TrainingPlannerApplication.java
@@ -0,0 +1,13 @@
+package hr.fer.trainingplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/fer/trainingplanner/TrainingPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.fer.trainingplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainingPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
